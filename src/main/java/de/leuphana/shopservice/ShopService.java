40e634cc10b64aa6909591ce035513b602cf759a
kevinmoisch <kevinmoisch@gmail.com>
2020-03-09 00:12:58 +0100

    Adding Endpoints

--- src/main/java/de/leuphana/shopservice/ShopService.java
+++ src/main/java/de/leuphana/shopservice/ShopService.java
@@ -1,6 +1,8 @@
 package de.leuphana.shopservice;
 
+import de.leuphana.articleservice.component.structure.Article;
 import de.leuphana.customerservice.component.structure.Customer;
+import de.leuphana.orderservice.component.structure.Order;
 import de.leuphana.shopservice.component.structure.LoginDetails;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -11,12 +13,15 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.BodyInserter;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 
 import javax.annotation.PostConstruct;
 import java.net.URI;
 import java.security.Key;
-import java.util.Collections;
+import java.util.*;
 
 
 @SpringBootApplication
@@ -60,4 +65,49 @@ public class ShopService {
         }
 
     }
+
+    public List<Article> getAllArticles () {
+        WebClient.RequestBodySpec request = this.webClient
+                .method(HttpMethod.GET)
+                .uri("/article/");
+
+        return request.retrieve().bodyToFlux(Article.class).collectList().block();
+    }
+
+    public Article updateArticleByArticleId(UUID articleId, Article article) {
+        WebClient.RequestHeadersSpec<?> request = this.webClient
+                .post()
+                .uri("/article/"+articleId.toString()+"/")
+                .body(BodyInserters.fromObject(article));
+
+        return request.retrieve().bodyToMono(Article.class).block();
+    }
+
+    public Article upsertArticle(Article article) {
+        WebClient.RequestHeadersSpec<?> request = this.webClient
+                .post()
+                .uri("/article/")
+                .body(BodyInserters.fromObject(article));
+
+        return request.retrieve().bodyToMono(Article.class).block();
+    }
+
+    public List<Order> getOrdersByCustomerId(UUID customerId) {
+        WebClient.RequestBodySpec request = this.webClient
+                .method(HttpMethod.GET)
+                .uri("/order/?customerId="+customerId);
+
+        return request.retrieve().bodyToFlux(Order.class).collectList().block();
+    }
+
+    public Order createOrder(Order order) {
+        WebClient.RequestHeadersSpec<?> request = this.webClient
+                .post()
+                .uri("/order/")
+                .body(BodyInserters.fromObject(order));
+
+        return request.retrieve().bodyToMono(Order.class).block();
+    }
+
+
 }
--- src/main/java/de/leuphana/shopservice/connector/ApiGatewayRestController.java
+++ src/main/java/de/leuphana/shopservice/connector/ApiGatewayRestController.java
@@ -1,6 +1,8 @@
 package de.leuphana.shopservice.connector;
 
+import de.leuphana.articleservice.component.structure.Article;
 import de.leuphana.customerservice.component.structure.Customer;
+import de.leuphana.orderservice.component.structure.Order;
 import de.leuphana.shopservice.ShopService;
 import de.leuphana.shopservice.component.structure.LoginDetails;
 import io.jsonwebtoken.Jwts;
@@ -10,6 +12,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Key;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
 
 @RestController
 public class ApiGatewayRestController {
@@ -27,11 +33,29 @@ public class ApiGatewayRestController {
     }
 
     @GetMapping("/api/v1/article")
-    public String getAllArticles() {
-        return "Test";
+    public List<Article> getAllArticles() {
+        return this.shopService.getAllArticles();
     }
 
+    @PostMapping("/api/v1/article/{articleId}/")
+    public Article updateArticle(@PathVariable("articleId") UUID articleId, @RequestBody Article article) {
+        return this.shopService.updateArticleByArticleId(articleId, article);
+    }
 
+    @PostMapping("/api/v1/article/")
+    public Article updateArticle(@RequestBody Article article) {
+        return this.shopService.upsertArticle(article);
+    }
+
+    @PostMapping("/order/")
+    public Order createOrder(@RequestBody Order order) {
+        return shopService.createOrder(order);
+    }
+
+    @GetMapping("/order/")
+    public List<Order> getOrdersByCustomerId(@RequestParam UUID customerId) {
+        return shopService.getOrdersByCustomerId(customerId);
+    }
 
 
 
